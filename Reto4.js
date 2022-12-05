eedaa2776a17139fe211c1e0bf6b45e5f0e5b75e
Kevin Zerpa <99708401+KitsuneQki@users.noreply.github.com>
2022-12-04 21:27:40 -0400

    Day 4

---
+++ Reto4.js
@@ -0,0 +1,8 @@
+function fitsInOneBox(boxes) {
+    return (() => boxes.every(({l,w,h}, idx) => {
+      if (idx === boxes.length - 1) return true
+      const nextBox = boxes[idx + 1] || {}
+      const canFitsIn = (nextBox.l > l && nextBox.w > w && nextBox.h > h) || (nextBox.l < l && nextBox.w < w && nextBox.h < h)
+      return canFitsIn
+    }))()
+  }
